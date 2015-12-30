23d054efcca4dd4a038ed611378358be685a525e
Gary Liu <desp0916@gmail.com>
2015-12-31 00:38:49 +0800

    Updated.

---
+++ src/main/java/learn/java/concurrent/Actor.java
@@ -0,0 +1,58 @@
+package learn.java.concurrent;
+
+public class Actor extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println(getName() + "是一个演员！");
+		int count = 0;
+		boolean keepRunning = true;
+		while (keepRunning) {
+			System.out.println(getName() + "登台演出：" + (++count));
+			if (count == 1000) {
+				keepRunning = false;
+			}
+			if (count % 10 == 0) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println(getName() + "的演出结束了！");
+	}
+
+	public static void main(String[] args) {
+		Thread actor = new Actor();
+		actor.setName("Mr. Thread");
+		actor.start();
+
+		Thread actressThread = new Thread(new Actress(), "Ms. Runnable");
+		actressThread.start();
+	}
+}
+
+class Actress implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + "是一个演员！");
+		int count = 0;
+		boolean keepRunning = true;
+		while (keepRunning) {
+			System.out.println(Thread.currentThread().getName() + "登台演出：" + (++count));
+			if (count == 1000) {
+				keepRunning = false;
+			}
+			if (count % 10 == 0) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println(Thread.currentThread().getName() + "的演出结束了！");
+	}
+}
