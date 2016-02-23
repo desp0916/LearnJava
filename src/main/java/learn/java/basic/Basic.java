558a8999d69e967f4e5495d2fec22cde7b698cba
劉祖弘 <gary@ryuu-sohiromumato-MacBook.local>
2016-02-24 00:38:13 +0800

    Updated.

---
+++ src/main/java/learn/java/basic/Basic.java
@@ -0,0 +1,13 @@
+package learn.java.basic;
+
+public class Basic {
+	public static void main(String[] args) {
+//		int i = (int) (1 + 1.1f + 1.1);
+//		long l = 1 + 1.1f + 1.1;	// cannot convert double to long
+//		int i = (int)1.1f + 1.1;	// cannot convert double to int
+		double d = 0.01;
+		long l = 1000L;
+		double dl = d + l;
+		System.out.println(i);
+	}
+}
